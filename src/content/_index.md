559ba6c74e5bbf01014d8af5d92e405b11e481c5
MiCh4n <cassanderburn@gmail.com>
2021-04-20 10:48:33 +0200

    Add sync-files script

--- src/content/_index.md
+++ src/content/_index.md
@@ -7,5 +7,5 @@ Hi! Here you can find info about site, my projects which im working on and also 
 
 Im hosting this website on VPS using nginx as webserver, hugo generates static html files. 
 Nginx expose statistics about his metrics, then nginx-prometheus-exporter consume data and "send" it to prometheus and after that Grafana is used to show data.
-Source code is stored in github remote repository, which sends webhooks to server, then triggers script to clone/pull repository and after that uses hugo to generate static html files which are moved to nginx site directory.
-Finally cleaning everything up to free storage
+Source code is stored in github remote repository, which sends webhooks to server, then triggers script to pull repository and after that uses hugo to generate static html files which are moved to nginx site directory.
+
---
+++ sync-files.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+## Lockfile command
+lock_file_or_dir="/var/lock/sync_site"
+cmd_locking="mkdir ${lock_file_or_dir}"
+cmd_check_lock="test -d ${lock_file_or_dir}"
+cmd_unlocking="rm -rf ${lock_file_or_dir}"
+
+## Manage Lockfile
+function is_running()
+{
+    local cmd_check_lock=${1}
+
+    ${cmd_check_lock} || {
+        return 1
+    }
+
+    return 0
+}
+
+function create_lock()
+{
+    local cmd_locking=${1}
+
+    ${cmd_locking} || {
+        printf "Cannot create lock\\n"
+        exit 2
+    }
+}
+
+function remove_lock()
+{
+    local cmd_unlocking="${1}"
+
+    ${cmd_unlocking} || {
+        printf "Cannot unlock\\n"
+        exit 3
+    }
+}
+
+trap 'remove_lock "${cmd_unlocking}"' SIGINT SIGTERM
+
+if is_running "${cmd_check_lock}"; then
+    printf "Cannot acquire lock -> exiting\\n"
+    exit 1
+fi
+
+create_lock "${cmd_locking}"
+
+
+## Important variables
+cmd_git_pull="git pull"
+cmd_hugo="hugo"
+cmd_pwd=$(pwd)
+cmd_rsync="rsync -av ${cmd_pwd}/src/public/ /var/www/tmp.bieda.it"
+cmd_rm_public="rm -r ${cmd_pwd}/src/public"
+reload_nginx="sudo systemctl reload nginx.service"
+## Script body
+if [ -d "$cmd_pwd/src/public" ]; then
+	${cmd_rm_public} || {
+		printf "Cannot remove public --> exiting" >&2 ;
+	  	remove_lock "${cmd_unlocking}"
+	    	exit 4
+	}
+fi
+
+${cmd_git_pull} || {
+	printf "Cannot pull repository --> exiting" >&2 ;
+  	remove_lock "${cmd_unlocking}"
+    	exit 5
+}
+
+${cmd_hugo} || {
+	printf "Cannot use hugo --> exiting" >&2 ;
+  	remove_lock "${cmd_unlocking}"
+    	exit 6
+}
+
+${cmd_rsync} || {
+	printf "Cannot rsync files --> exiting" >&2 ;
+  	remove_lock "${cmd_unlocking}"
+    	exit 7
+}
+
+${cmd_rm_public} || {
+	printf "Cannot create secret --> exiting" >&2 ;
+  	remove_lock "${cmd_unlocking}"
+    	exit 8
+}
+
+${reload_nginx} || {
+	printf "Cannot create secret --> exiting" >&2 ;
+  	remove_lock "${cmd_unlocking}"
+    	exit 9
+}
+## end body
+
+remove_lock "${cmd_unlocking}"
